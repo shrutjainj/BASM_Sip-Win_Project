022921ab835ef1b776a32a5dc927ad03d5ee31f2
Shrut jain <shrutjain432@gmail.com>
2026-06-20 02:14:44 +0530

    Fix api urls

--- api/urls.py
+++ api/urls.py
@@ -3,27 +3,12 @@ from .views import (
     scan_qr,
     redeem_coupon,
     analytics,
-    coupon_report
-)
-
-# 🔐 JWT
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    coupon_report,
 )
 
 urlpatterns = [
-
     path('scan/', scan_qr),
-    
     path('redeem/', redeem_coupon),
-
     path('analytics/', analytics),
     path('report/', coupon_report),
-
-    # 🔐 JWT Login
-    path('login/', TokenObtainPairView.as_view()),
-    
-    # 🔄 Refresh token
-    path('refresh/', TokenRefreshView.as_view()),
 ]
